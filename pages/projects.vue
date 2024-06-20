52d26bf8a4d4408770e474fdefb5fec87204ab9d
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2024-06-20 13:25:33 +0300

    netlify

--- pages/projects.vue
+++ pages/projects.vue
@@ -16,18 +16,13 @@
 <script>
 import { mapState } from 'vuex'
 export default {
-  async asyncData({store}) {
-   /* const dataset = await store.dispatch('projects/getAll')
-   return {dataset} */
-   try {
-       const response = await fetch('/.netlify/functions/projects');
+  async fetch() {
+    const response = await fetch('/.netlify/functions/projects');
        const dataset = await response.json();
        console.log(dataset); // Массив проектов из MongoDB
        return {dataset}
-     } catch (error) {
-       console.error('Ошибка при получении проектов:', error);
-     }
   },
+  
   data() {
     return {
       activeNames: [false]

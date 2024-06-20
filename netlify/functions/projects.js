c04217a595fd8545507488f20544a95a65e549b1
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2024-06-20 09:01:38 +0300

    netlify

--- netlify/functions/projects.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const keys = require('./keys')
-const Project = require('../models/project.model')
-
-mongoose.connect(keys.MONGO_URI, {}) 
-  .then(() => console.log('MongoDB connected'))
-  .catch((e) => console.log(e))
-
-mongoose.set('strictQuery', false);

0a22bc26fffc254b21d61f7a13ba3bdcbf3372ab
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2024-06-20 08:57:16 +0300

    netlify

--- netlify/functions/projects.js
+++ netlify/functions/projects.js
@@ -1,31 +1,16 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose')
+const keys = require('./keys')
+const Project = require('../models/project.model')
 
-const uri = process.env.MONGO_URI; // Получаем URI из переменной окружения
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(keys.MONGO_URI, {}) 
+  .then(() => console.log('MongoDB connected'))
+  .catch((e) => console.log(e))
 
-exports.handler = async function(event, context) {
-  try {
-    // Подключаемся к MongoDB
-    await client.connect();
-    const database = client.db('myFirstDatabase'); // Замените 'your-database-name' на имя вашей базы данных
-    const collection = database.collection('project'); // Замените 'projects' на имя вашей коллекции
+mongoose.set('strictQuery', false);
 
-    // Получаем данные из MongoDB (например, все проекты)
-    const projects = await collection.find({}).toArray();
-
-    // Возвращаем данные в формате JSON
-    return {
-      statusCode: 200,
-      body: JSON.stringify(projects),
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to fetch projects' }),
-    };
-  } finally {
-    // Закрываем соединение с MongoDB
-    await client.close();
-  }
-};
+try {
+  const projects = await Project.find().sort({prsort: 1})
+  res.status(200).json(projects)
+} catch (error) {
+  res.status(500).json(error)
+}

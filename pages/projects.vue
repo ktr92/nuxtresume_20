d0e9b6a9bfd7d8e054f96670a9033f87894f61cb
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2024-06-20 08:50:19 +0300

    netlify

--- pages/projects.vue
+++ pages/projects.vue
@@ -16,7 +16,7 @@
 <script>
 import { mapState } from 'vuex'
 export default {
-  async asyncData({store}) {
+ /*  async asyncData({store}) {
    const dataset = await store.dispatch('projects/getAll')
    return {dataset}
   },
@@ -29,8 +29,20 @@ export default {
     ...mapState([
       'about'
     ])
-  }
+  } */
+    async asyncData() {
+      try {
+        const response = await fetch('/.netlify/functions/projects');
+        const dataset = await response.json();
+        return {dataset}
+      } catch (error) {
+        console.error('Ошибка при получении проектов:', error);
+      }
+    }
+
 }
+
+
 </script>
 
 <style scoped lang="scss">

622103f6a5b9220b474491651bbc71f795f27c8b
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-05-14 17:44:09 +0300

    view

--- components/AppProject.vue
+++ components/AppProject.vue
@@ -1,51 +1,53 @@
 <template>
-    <el-collapse-item :name="data.prname">
-      <template slot="title" >
-          <div class="projects__header clearfix">
-            <span class="projects__title">{{ data.prname }}</span>
-            <span class="projects__images">
-              <span v-for="prtag in data.prtags" :key="prtag" class="tags-images">
-                <span v-if="tags[prtag]">
-                  <img :src="require(`~/assets/img/${tags[prtag].icon}`)" />
-                  <span>{{ tags[prtag].name}} </span>
-                </span>
-              </span>
+  <el-collapse-item :name="data.prname">
+    <template slot="title">
+      <div class="projects__header clearfix">
+        <span class="projects__title">{{ data.prname }}</span>
+        <span class="projects__images">
+          <span v-for="prtag in data.prtags" :key="prtag" class="tags-images">
+            <span v-if="tags[prtag]">
+              <img :src="require(`~/assets/img/${tags[prtag].icon}`)" />
+              <span>{{ tags[prtag].name }} </span>
             </span>
-          </div>
-            </template> 
-             <el-button-group>
-            <el-button type="primary" v-if="data.prgit">
-              <a v-if="data.prgit === '0'" href="#">Github (по запросу)</a> 
-              <a v-else :href="data.prgit" target="_blank">GitHub</a> 
-            </el-button>
-            <el-button type="primary" v-if="data.prlink">
-              <a :href="data.prlink" target="_blank">Демо</a> 
-            </el-button>
-            <el-button type="primary" v-if="data.prwork">
-              <a :href="data.prwork" target="_blank">Сайт</a> 
-            </el-button>
-          </el-button-group>
-          <div class="text item projects__text">
-              <p><vue-markdown>{{ data.prdescription }}</vue-markdown></p>
-          </div>
-         
-    </el-collapse-item>
+          </span>
+        </span>
+      </div>
+    </template>
+    <div>
+      <el-button-group>
+        <el-button type="primary" v-if="data.prgit">
+          <a v-if="data.prgit === '0'" href="#">Github (по запросу)</a>
+          <a v-else :href="data.prgit" target="_blank">GitHub</a>
+        </el-button>
+        <el-button type="primary" v-if="data.prlink">
+          <a :href="data.prlink" target="_blank">Демо</a>
+        </el-button>
+        <el-button type="primary" v-if="data.prwork">
+          <a :href="data.prwork" target="_blank">Сайт</a>
+        </el-button>
+      </el-button-group>
+    </div>
+
+    <div class="text item projects__text">
+      <p>
+        <vue-markdown>{{ data.prdescription }}</vue-markdown>
+      </p>
+    </div>
+  </el-collapse-item>
 </template>
 
 <script>
 export default {
-  props: ['data'],
-    data() {
+  props: ["data"],
+  data() {
     return {
       tags: [],
-    }
+    };
   },
   mounted() {
-    this.tags = this.$store.getters['projects/tags']
+    this.tags = this.$store.getters["projects/tags"];
   },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
